12f88cda8cfd41615b93a0fb7d70afc3d024df14
NT\AbedAllateef.ALQaisi <AbedAllateef.Alqaisi@jo.zain.com>
2025-10-05 10:41:58 +0300

    working on screens

--- lib/src/layout/zj_screen_with_action_button.dart
+++ lib/src/layout/zj_screen_with_action_button.dart
@@ -13,50 +13,59 @@ class ZjScreenWithActionButtonWrapper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final theme = Theme.of(context).zjTheme;
 
     return Scaffold(
-      // backgroundColor: Colors.white, // default white background
-      body: Padding(
-        padding: const EdgeInsets.all(ZjComponentsUnits.screenWithActionButtonPadding), // default padding
-        child: Container(
-          //color: Colors.black,
-          //margin: const EdgeInsets.all(20.0), // default margin
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            color: theme.screenWithActionButtonBackgroundColor, // background
-            borderRadius: BorderRadius.circular(ZjUnits.rmd), // border-radius
-            border: Border.all(
-              color: theme.screenWithActionButtonBorderColor, // border
-              width: 1,
-            ),
+      body: SafeArea(
+        bottom: false, // 👈 Avoid double padding with bottomNavigationBar
+        child: Padding(
+          padding: const EdgeInsets.all(
+            ZjComponentsUnits.screenWithActionButtonPadding,
           ),
-          child: child,
+          // 👇 Added LayoutBuilder to make scroll + full height work together
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              return SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(minHeight: constraints.maxHeight),
+                  child: Container(
+                    //alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      color: theme.screenWithActionButtonBackgroundColor,
+                      borderRadius: BorderRadius.circular(ZjUnits.rmd),
+                      border: Border.all(
+                        color: theme.screenWithActionButtonBorderColor,
+                        width: 1,
+                      ),
+                    ),
+                    child: child,
+                  ),
+                ),
+              );
+            },
+          ),
         ),
       ),
       bottomNavigationBar: actionButton != null
-          ? Container(
-              padding: const EdgeInsets.all(ZjComponentsUnits.screenWithActionButtonContainerPadding),
-              decoration: BoxDecoration(
-                color: theme.screenWithActionButtonContainerColor,
-                border: Border(
-                  top: BorderSide(
-                    color: theme.screenWithActionButtonBorderColor, // border-top
-                    width: 1,
-                  ),
-                ),
+          ? SafeArea(
+        top: false,
+        child: Container(
+          padding: const EdgeInsets.all(
+            ZjComponentsUnits.screenWithActionButtonContainerPadding,
+          ),
+          decoration: BoxDecoration(
+            color: theme.screenWithActionButtonContainerColor,
+            border: Border(
+              top: BorderSide(
+                color: theme.screenWithActionButtonBorderColor,
+                width: 1,
               ),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                 mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  actionButton!,
-                  //const SizedBox(height: ZjComponentsUnits.screenWithActionButtonContainerGap), // gap
-                ],
-              ),
-            )
+            ),
+          ),
+          child: actionButton!,
+        ),
+      )
           : null,
     );
   }
